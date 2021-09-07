7fe474bc74740021d6ca4bfba225557a6eb92be3
Ayan <ayanbanerjee95@outlook.com>
2021-09-08 01:28:36 +0530

    homepage index

--- src/components/Homepage/index.jsx
+++ src/components/Homepage/index.jsx
@@ -1,28 +1,17 @@
 import React, { useState, useEffect, useRef } from "react";
-import {
-    StyledWrapper,
-    BannerImage,
-    StyledBannerTextContainer,
-    StyledBannerHeader,
-    StyledBannerHeaderHighlight,
-    StyledBannerDesc,
-    StyledSectionLite,
-    StyledSectionWrapper,
-    StyledSectionHeader,
-    HeroModule
-} from "./style";
+
 import { StyledDiv, StyledMaxWidthSection } from "../../style";
-import HeaderCarousel from "./components/Header Carousel";
-import ScrollingHeader, { Panel } from "./components/Scrolling Header";
+import ScrollingHeader from "./components/Scrolling Header";
+import Section from "./components/Section";
 import VerticalCarousel from "./components/Section Carousel";
 import data1 from "./components/Scrolling Header/data.json";
 import data2 from "./components/Section Carousel/data.json";
 
 const HomePage = (props) => {
-    console.log(process.env.PUBLIC_URL);
     return (
         <StyledDiv>
             <ScrollingHeader data={data1} />
+            <Section />
             <VerticalCarousel
                 data={data2.slides}
                 leadingText={data2.leadingText}
