ee5564f5c8b3f5263eaf4921dab203eaec9554d1
Ayan <ayanbanerjee95@outlook.com>
2021-09-13 23:39:46 +0530

    solution section

--- src/components/Homepage/components/Solution Section/index.jsx
+++ src/components/Homepage/components/Solution Section/index.jsx
@@ -1,28 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyledMaxWidthSection } from "../../../../style";
 import Title from "../../../Shared/title";
 import { Section, SectionBg } from "./styles";
 import Column from "./column";
+import Selector from "./selector";
 import data from "./data.json";
 const Solution = (props) => {
+    const [selectFirst, setSelectFirst] = useState(true);
     return (
-        <SectionBg>
+        <div style={{ overflow: "hidden", width: "200%", display: "flex" }}>
             <Title color="s" text="Let's see how we can solve them..." />
-            <StyledMaxWidthSection maxWidth={true}>
-                <Section>
-                    {data.map((e) => {
-                        return (
-                            <Column
-                                flow={e.flow}
-                                title={e.title}
-                                bodyFront={e.bodyFront}
-                                bodyBack={e.bodyBack}
-                            />
-                        );
-                    })}
-                </Section>
-            </StyledMaxWidthSection>
-        </SectionBg>
+            <Selector
+                selectFirst={selectFirst}
+                setSelectFirst={setSelectFirst}
+            />
+            <SectionBg selectFirst={selectFirst}>
+                <StyledMaxWidthSection maxWidth={true}>
+                    <Section>
+                        {data.map((e) => {
+                            return (
+                                <Column
+                                    flow={e.flow}
+                                    title={e.title}
+                                    bodyFront={e.bodyFront}
+                                    bodyBack={e.bodyBack}
+                                />
+                            );
+                        })}
+                    </Section>
+                </StyledMaxWidthSection>
+            </SectionBg>
+            <SectionBg selectFirst={selectFirst}>
+                <StyledMaxWidthSection maxWidth={true}>
+                    <Section>
+                        {data.map((e) => {
+                            return (
+                                <Column
+                                    flow={e.flow}
+                                    title={e.title}
+                                    bodyFront={e.bodyFront}
+                                    bodyBack={e.bodyBack}
+                                />
+                            );
+                        })}
+                    </Section>
+                </StyledMaxWidthSection>
+            </SectionBg>
+        </div>
     );
 };
 
