aa0eef13cb4e4c89956bf5b41defb5629dfb4eae
Ayan <ayanbanerjee95@outlook.com>
2021-09-17 21:07:37 +0530

    contact form

---
+++ src/components/Homepage/components/ContactSection/ContactForm/index.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { Wrapper, TitleWrapper, Title } from "../styles";
+import Form from "./Form";
+
+const ContactForm = () => {
+    return (
+        <Wrapper>
+            <TitleWrapper>
+                <Title>Everything that we offer...</Title>
+            </TitleWrapper>
+        </Wrapper>
+    );
+};
+
+export default ContactForm;
